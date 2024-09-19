eab649561560f6c1f489ef17eb41e2f204e42ccc
shafiq <shafiqahmad081@gmail.com>
2024-09-19 07:35:26 +0600

    create safetalk project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./**/*.{js}"],
+  theme: {
+    fontFamily: {
+      body: ['"Montserrat", sans-serif'],
+      open: ['"Open Sans", sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
