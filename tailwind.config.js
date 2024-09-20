ab3013933b4008cdce5a3505bfbb3346b08869a3
shafiq <shafiqahmad081@gmail.com>
2024-09-20 08:36:36 +0600

    update animation section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,42 @@ module.exports = {
       body: ['"Montserrat", sans-serif'],
       open: ['"Open Sans", sans-serif'],
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        moveEl: {
+          "100%": {
+            transform:
+              "translateX(calc(var(--item-possition) * 20px)) translateY(calc(var(--item-possitionY) * 15px))",
+          },
+        },
+        increaseWidth: {
+          "100%": {
+            width: "324px",
+          },
+        },
+        increaseFontSize: {
+          "100%": {
+            fontSize: "1.375rem",
+          },
+        },
+        zoom: {
+          "100%": {
+            transform: "scale(1)",
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        moveEl: "moveEl 1s linear",
+        increaseWidth: "increaseWidth 1s linear",
+        increaseFontSize: "increaseFontSize 1s linear",
+        zoom: "zoom 1s linear",
+      },
+    },
   },
   plugins: [],
 };
+
+// Move to targated possition
+// Increase targated width
+// Increase font size
